811aec20ddceb5da98d51da119d80e7ebb666878
Pedro Farias <pedrobfarias@gmail.com>
2021-05-04 00:52:05 -0300

    Added Interactions

--- public/Perseia.js
+++ public/Perseia.js
@@ -2,8 +2,16 @@ class Perseia {
 
     constructor() {
 
+        this.layersName = [
+            'ccja_andar_1_0'
+        ]
+
+        this.createQgis2webPolyFill();
+        this.initElements();
         this.initMap();
-        this.toggleFullScreen();
+        this.createClickInteration();
+        this.createLayers();
+        this.registerEvents();
 
     }
 
@@ -11,17 +19,17 @@ class Perseia {
 
         this.map = new ol.Map({
             layers: [
-                new ol.layer.Image({
-                    source: new ol.source.ImageWMS({
-                        url: 'http://mapas.geomatica.ufpr.br/geoserver/wms',
-                        params: { 'LAYERS': 'UCM-BASE:ccja_base_campus,UCM-BASE:ccja_andar_0' },
-                        ratio: 1,
-                        serverType: 'geoserver',
-                    }),
-                }),
-                // new ol.layer.Tile({
-                //     source: new ol.source.OSM()
-                // })
+                // new ol.layer.Image({
+                //     source: new ol.source.ImageWMS({
+                //         url: 'http://mapas.geomatica.ufpr.br/geoserver/wms',
+                //         params: { 'LAYERS': 'UCM-BASE:ccja_base_campus,UCM-BASE:ccja_andar_0' },
+                //         ratio: 1,
+                //         serverType: 'geoserver',
+                //     }),
+                // }),
+                new ol.layer.Tile({
+                    source: new ol.source.OSM()
+                })
             ],
             target: 'map',
             view: new ol.View({
@@ -32,9 +40,118 @@ class Perseia {
 
     }
 
-    toggleFullScreen() {
+    initElements() {
+
+        this.startBtn = document.getElementById('start');
+        this.disclaimerElm = document.getElementById('disclaimer');
+        this.mapElm = document.getElementById('map');
+
+    }
+
+
+    createLayers() {
+
+        for (let i = 0; i < this.layersName.length; i++) {
+            const layerName = this.layersName[i];
+            this.createGsonLayer(layerName);
+        }
+
+    }
+
+    createGsonLayer(layerName) {
+
+        let format = new ol.format.GeoJSON();
+        let features = format.readFeatures(window[`json_${layerName}`], {
+            dataProjection: 'EPSG:4326',
+            featureProjection: 'EPSG:3857'
+        });
+        let source = new ol.source.Vector();
+        source.addFeatures(features);
+        let layer = new ol.layer.Vector({
+            source: source,
+            style: window[`style_${layerName}`],
+            interactive: true
+        });
+        this.map.addLayer(layer);
+
+    }
+
+    createClickInteration() {
+
+        this.clickInteration = new ol.interaction.Select();
+        this.map.addInteraction(this.clickInteration);
+
+    }
+
+    startFullScreen() {
 
         document.documentElement.requestFullscreen();
+
+    }
+
+    showFeatureInfo(feature) {
+
+        console.log(feature)
+
+        if (this.overlay) this.map.removeOverlay(this.overlay);
+
+        let width = getComputedStyle(this.mapElm).getPropertyValue("width").replace('px', '');
+        let center = this.map.getView().getCenter();
+        let boxElm = document.createElement('div');
+
+        boxElm.style.width = width * 0.7 + 'px';
+        boxElm.className = 'box';
+        boxElm.innerHTML = `
+            <h3>${feature.getProperties().ambiente}</h3>
+            <p>Você selecionou como destino "${feature.getProperties().ambiente}".</p>
+            <p>Deseja finalizar o teste?</p><br>
+            <div>
+                <a id="box-cancel" href="#">Cancelar</a> 
+                <a id="box-finish" class="finish" href="#">Finalizar</a>
+            </div>
+        `;
+
+        this.overlay = new ol.Overlay({
+            element: boxElm,
+            positioning: 'center-center',
+            position: center
+        });
+        this.map.addOverlay(this.overlay);
+
+
+        document.getElementById('box-cancel').addEventListener('click', () => {
+            this.map.removeOverlay(this.overlay);
+        });
+
+        document.getElementById('box-finish').addEventListener('click', () => {
+            this.map.removeOverlay(this.overlay);
+        });
+
+
+    }
+
+    registerEvents() {
+
+        this.startBtn.addEventListener('click', () => {
+
+            this.disclaimerElm.style.display = 'none';
+            this.startFullScreen();
+
+        });
+
+        this.clickInteration.on('select', evt => {
+
+            this.showFeatureInfo(evt.selected[0]);
+            this.clickInteration.getFeatures().clear();
+
+        });
+
+
+    }
+
+    createQgis2webPolyFill() {
+
+        window.createTextStyle = function () { };
 
     }
 
---
+++ public/styles/ccja_andar_1_0_style.js
@@ -0,0 +1,144 @@
+var size = 0;
+var placement = 'point';
+function categories_ccja_andar_1_0(feature, value, size, resolution, labelText,
+                       labelFont, labelFill, bufferColor, bufferWidth,
+                       placement) {
+                switch(value.toString()) {case 'Banheiro':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}),fill: new ol.style.Fill({color: 'rgba(86,230,226,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Circulação':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}),fill: new ol.style.Fill({color: 'rgba(200,183,112,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Construção':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}),fill: new ol.style.Fill({color: 'rgba(98,124,237,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Ensino':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}),fill: new ol.style.Fill({color: 'rgba(56,155,216,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Institutos de Educação':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}),fill: new ol.style.Fill({color: 'rgba(236,113,36,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Objeto':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}),fill: new ol.style.Fill({color: 'rgba(215,27,196,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Objetos':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}),fill: new ol.style.Fill({color: 'rgba(16,216,119,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Sala de Professores':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}),fill: new ol.style.Fill({color: 'rgba(146,121,219,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Transição de Nível':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}),fill: new ol.style.Fill({color: 'rgba(102,200,52,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Uso Administrativo':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}),fill: new ol.style.Fill({color: 'rgba(198,127,234,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Uso Comercial':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}),fill: new ol.style.Fill({color: 'rgba(96,214,106,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Uso Comum':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}),fill: new ol.style.Fill({color: 'rgba(211,235,131,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Vazio':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}),fill: new ol.style.Fill({color: 'rgba(232,97,163,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+default:
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}),fill: new ol.style.Fill({color: 'rgba(204,63,70,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;}};
+
+var style_ccja_andar_1_0 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    var value = feature.get("tipoAmb");
+    var labelText = "";
+    size = 0;
+    var labelFont = "10px, sans-serif";
+    var labelFill = "#000000";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 8;
+    var offsetY = 3;
+    var placement = 'point';
+    if ("" !== null) {
+        labelText = String("");
+    }
+    
+var style = categories_ccja_andar_1_0(feature, value, size, resolution, labelText,
+                          labelFont, labelFill, bufferColor,
+                          bufferWidth, placement);
+
+    return style;
+};
